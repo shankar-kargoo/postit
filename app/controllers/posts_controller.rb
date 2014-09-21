bbbe882f37fcd81108bd3103497712c98179f5e9
Shankar Narayan <shankar@kargoo.com>
2014-09-21 03:02:09 -0700

    added posts create and edit functionality including forms for the same

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,16 +8,40 @@ class PostsController < ApplicationController
  		@post = Post.find(params[:id])
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Your Post has been updated."
+      redirect_to posts_path
+    else
+      @post.errors
+      render :edit      
+    end
   end
-  
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      @post.errors
+      render :new
+    end
+  end
+
+private
+
+def post_params
+   params.require(:post).permit!
+end
+
 end
